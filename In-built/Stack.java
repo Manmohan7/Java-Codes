8661573ced04fd5f133140ed3321017097b3d163
Manmohan7 <er.manmohansingh7@gmail.com>
2017-04-26 11:58:59 +0530

    Stack with Output

--- In-built/Stack.java
+++ In-built/Stack.java
@@ -1,29 +1,37 @@
 import java.util.Stack;
+import java.lang.String;
 
 class stack
 {
     public static void main(String[] args)
     {
-        Stack s = new Stack();
+        // constructor
+        Stack s = new Stack();    // creates an empty stack
+        System.out.println("New Stack " + s);
 
         // empty() - returns boolean
-        System.out.println(s.empty());
+        System.out.println("Check Empty " + s.empty());
 
         // push(element) - returns the element entered
-        s.push(1);
+        // pushes the element on the top of stack
+        s.push(1);    
         s.push(5);
         s.push(9);
         s.push(4);
         s.push(7);
-        System.out.println(s.push(2));
+        System.out.println("Push " + s.push(2));
 
         // peek() - returns object on top of stack
-        System.out.println(s.peek());
+        // does not remove the element
+        // throws EmptyStackException if stack is empty
+        System.out.println("Peek element " + s.peek());
 
         // pop() - returns object on top of stack
-        System.out.println(s.pop());
+        // removes the element from the stack
+        // throws EmptyStackException is stack is empty
+        System.out.println("Popped Element " + s.pop());
 
         // search(element) - returns int counting from 1 and top of stack
-        System.out.println(s.search(2));    // if not found returns -1
+        System.out.println("Search Element Position " + s.search(2));    // if not found returns -1
     }	
 }
